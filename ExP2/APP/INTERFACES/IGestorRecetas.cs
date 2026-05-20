56cfb6f78f71cc3fcc4b3ef1b813f875b480fef3
LuisRomero0 <luissrc.fi@gmail.com>
2026-05-20 09:47:11 -0600

    CORRECCIÓN DE ERRORES DESPUES DE ENTREGA

--- ExP2/APP/INTERFACES/IGestorRecetas.cs
+++ ExP2/APP/INTERFACES/IGestorRecetas.cs
@@ -1,24 +1,52 @@
-﻿namespace APP.INTERFACES
+﻿using APP.MODELOS;
+
+namespace APP.INTERFACES
+
 {
     public class IGestorRecetas // Interface gestor -> Contrato con el gestor
     {
         // Propiedades
         public List<Receta> RecetasDisponibles { get; set; }
+        
+        public void AgregarReceta(Receta receta)
+        {
 
-        // -------------------------------------------------------------------------- Metodos ¿ LLevaria Metodos?
-        /*
-    //    public void AgregarReceta(Receta receta);
-    //    public void EliminarReceta(Receta receta);
-    //    public void EliminarPorIndice(int indice);
-    //    public List<Receta> BuscarPorNombre(string nombre);
-    //    public void LimpiarCatalogo();
-    //    public void QuickSort(List<Receta> lista);
+        }
+        public void EliminarReceta(Receta receta)
+        {
 
+        }
+        public void EliminarPorIndice(int indice)
+        {
 
-    //    public Receta BuscarPorNombre(string nombre); // Implementará Búsqueda Binaria
+        }
+        public List<Receta> BuscarPorNombre(string nombre)
+        {
+
+        }
+        public void LimpiarCatalogo()
+        {
+
+        }
+        public void QuickSort(List<Receta> lista)
+        {
+
+        }
+
+        public void MergeSort(Receta receta)
+        {
+
+        }
+
+        public void BusquedaBinaria(string nombre)
+        {
+
+        }
+
+        // public Receta BuscarPorNombre(string nombre); // Implementará Búsqueda Binaria
 
         // Aqui hay problemas
-    //    public void OrdenarPorTiempo(); // Implementará QuickSort o MergeSort
-        */
+        public void OrdenarPorTiempo() { } // Implementará QuickSort o MergeSort
+        
     }
 }
--- ExP2/APP/SERVICIOS/ExportadorTxT.cs
+++ ExP2/APP/SERVICIOS/ExportadorTxT.cs
@@ -3,50 +3,46 @@ using APP.MODELOS;
 
 namespace APP.SERVICIOS
 {
-    public class ExportadorTxT : IExportador 
+    public class ExportadorTxT : IExportador
     {
-            public bool ExportarATxt(Usuario usuario, string nombreLibro)
+        public bool ExportarATxt(Usuario usuario, string nombreLibro)
+        {
+            if (usuario == null) throw new ArgumentNullException(nameof(usuario));
+            if (string.IsNullOrWhiteSpace(nombreLibro)) throw new ArgumentException("El libro no puede estar vacío.");
+
+            // Validamos que el usuario realmente sea dueño del libro de recetas
+            if (!usuario.LibrosRecetas.ContainsKey(nombreLibro))
             {
-                if (usuario == null) throw new ArgumentNullException(nameof(usuario));
-                if (string.IsNullOrWhiteSpace(nombreLibro)) throw new ArgumentException("El libro no puede estar vacío.");
+                return false;
+            }
 
-                // Validamos que el usuario realmente sea dueño del libro de recetas
-                if (!usuario.LibrosRecetas.ContainsKey(nombreLibro))
+            try
+            {
+                // Nombre del archivo que exige el caso de prueba
+                string rutaArchivo = $"{usuario.Nombre}_{nombreLibro}.txt";
+                var recetas = usuario.LibrosRecetas[nombreLibro];
+
+                // Escritura física del archivo .txt
+                using (StreamWriter writer = new StreamWriter(rutaArchivo))
                 {
-                    return false;
-                }
+                    // Cumplimos con el test: "ContieneNombreUsuario"
+                    writer.WriteLine($"Usuario Propietario: {usuario.Nombre}");
+                    writer.WriteLine($"Libro de Recetas: {nombreLibro}");
 
-                try
-                {
-                    // Nombre del archivo que exige el caso de prueba
-                    string rutaArchivo = $"{usuario.Nombre}_{nombreLibro}.txt";
-                    var recetas = usuario.LibrosRecetas[nombreLibro];
-
-                    // Escritura física del archivo .txt
-                    using (StreamWriter writer = new StreamWriter(rutaArchivo))
+                    foreach (var receta in recetas)
                     {
-                        // Cumplimos con el test: "ContieneNombreUsuario"
-                        writer.WriteLine($"Usuario Propietario: {usuario.Nombre}");
-                        writer.WriteLine($"Libro de Recetas: {nombreLibro}");
-
-                        foreach (var receta in recetas)
-                        {
-                            // Cumplimos con el test: "ContieneNombreReceta"
-                            writer.WriteLine($"- Receta: {receta.Nombre} | Chef: {receta.Chef} | Duración: {receta.TiempoMinutos} min");
-                        }
+                        // Cumplimos con el test: "ContieneNombreReceta"
+                        writer.WriteLine($"- Receta: {receta.Nombre} | Chef: {receta.Chef} | Duración: {receta.TiempoMinutos} min");
                     }
+                }
 
-                    return true; // Si se creó el archivo con éxito, retorna true (Pasa el test 'CreaArchivo')
-                }
-                catch (Exception)
-                {
-                    return false;
-                }
+                return true; // Si se creó el archivo con éxito, retorna true (Pasa el test 'CreaArchivo')
+            }
+            catch (Exception)
+            {
+                return false;
             }
         }
+
     }
-
-
-
-}
 }
